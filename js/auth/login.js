2023083e326f0714bafb7a06155c631fc9bb85fd
unknown <cuevasrogelyn2@gmail.com>
2025-04-21 11:49:47 +0800

    Fix login form submission and mixed content issues

--- js/auth/login.js
+++ js/auth/login.js
@@ -16,6 +16,7 @@ document.getElementById("form_login").onsubmit = async (e) => {
   try {
     const response = await fetch(`${backendURL}/api/login`, {
       method: "POST",
+      mode: "no-cors",
       headers: {
         Accept: "application/json",
       },

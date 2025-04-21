a84096fd99c6e669b2e15007b716f346cdf4ec6b
unknown <cuevasrogelyn2@gmail.com>
2025-04-21 09:28:58 +0800

    Fix login form submission and mixed content issues

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -3,7 +3,7 @@ import { setRouter } from "../router/router.js";
 // Set Router
 setRouter();
 
-// Backend URL
+// Set the backend URL
 const backendURL = "https://capstone-system-production.up.railway.app";
 
 // Function to handle response
